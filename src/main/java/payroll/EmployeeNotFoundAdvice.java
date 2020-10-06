1ce514b371dcfa3edb848694711a56f8fa83dc14
Valentin Segundo Ratti <vratti@itba.edu.ar>
2020-10-06 14:35:09 -0300

    REST Spring Boot tuto

---
+++ src/main/java/payroll/EmployeeNotFoundAdvice.java
@@ -0,0 +1,15 @@
+package payroll;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class EmployeeNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(EmployeeNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
---
+++ src/main/java/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package payroll;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeNotFoundException extends RuntimeException {
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
