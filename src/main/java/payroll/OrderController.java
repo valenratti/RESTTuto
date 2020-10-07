d73c4e0320bd7f32e314b6067aa8f106b002101f
Valentin Segundo Ratti <vratti@itba.edu.ar>
2020-10-06 21:55:04 -0300

    "Finished RestTuto"

--- src/main/java/payroll/OrderController.java
+++ src/main/java/payroll/OrderController.java
@@ -2,11 +2,12 @@ package payroll;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,4 +56,45 @@ public class OrderController {
                 .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri()) //
                 .body(assembler.toModel(newOrder));
     }
+
+    @DeleteMapping("/orders/{id}/cancel")
+    ResponseEntity<?> cancel(@PathVariable Long id) {
+
+        Order order = orderRepository.findById(id) //
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.CANCELLED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity //
+                .status(HttpStatus.METHOD_NOT_ALLOWED) //
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
+                .body(Problem.create() //
+                        .withTitle("Method not allowed") //
+                        .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
+    }
+
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+
+        Order order = orderRepository.findById(id) //
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity //
+                .status(HttpStatus.METHOD_NOT_ALLOWED) //
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
+                .body(Problem.create() //
+                        .withTitle("Method not allowed") //
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
+    }
+
 }
+
+
---
+++ src/main/java/payroll/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package payroll;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(Long id) {
+        super("Could not find order " + id);
+    }
+}
